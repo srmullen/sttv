519b5d002174304686502d15f7a2724b6e255f5e
Sean Mullen <srmullen13@gmail.com>
2021-03-23 13:27:22 -0400

    cleanup

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,6 @@
 import { defineConfig } from 'vite'
 import svelte from '@svitejs/vite-plugin-svelte'
 
-console.log(process.env.NODE_ENV);
-
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [svelte()]
